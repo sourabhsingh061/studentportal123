8c101b2881a1c8ee12c389fe2d7d9d39ee64509f
sourabhrajputt <sourabhrajput.it@gmail.com>
2024-09-06 16:00:04 +0530

    done

---
+++ projectORS/src/main/java/com/rays/dao/CourseDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.CourseDTO;
+import com.rays.dto.StudentDTO;
+/**
+ * @author Shailendra Rai
+ *
+ */
+public interface CourseDAOInt extends BaseDAOInt<CourseDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Shailendra Rai
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
